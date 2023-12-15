3efec52add5c75ab8d7166e75d504d7c945ca8e3
dennis647 <dennis.brokking@gmail.com>
2023-12-15 14:00:22 +0100

    Initial commit

---
+++ src/test/java/org/kristiania/FinanceAppTests.java
@@ -0,0 +1,13 @@
+package org.kristiania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
