3a81a3d46fafcab3b0a00d3b8bc863fd8abec493
dennis647 <dennis.brokking@gmail.com>
2023-12-15 15:18:08 +0100

    Made ExpenseCategoryService api

--- src/main/java/org/kristiania/ExpenseCategoryService.java
+++ src/main/java/org/kristiania/ExpenseCategoryService.java
@@ -17,12 +17,28 @@ public class ExpenseCategoryService {
     static {
         CATEGORY_KEYWORDS.put("food", "Food");
         CATEGORY_KEYWORDS.put("mat", "Food");
+        CATEGORY_KEYWORDS.put("groceries", "Food");
+
         CATEGORY_KEYWORDS.put("rent", "Rent");
         CATEGORY_KEYWORDS.put("housing", "Rent");
+
         CATEGORY_KEYWORDS.put("transport", "Transportation");
         CATEGORY_KEYWORDS.put("car", "Transportation");
+
         CATEGORY_KEYWORDS.put("bills", "Bills");
         CATEGORY_KEYWORDS.put("electricity", "Bills");
+        CATEGORY_KEYWORDS.put("heating", "Bills");
+
+        CATEGORY_KEYWORDS.put("beer", "Alcohol");
+        CATEGORY_KEYWORDS.put("wine", "Alcohol");
+
+        CATEGORY_KEYWORDS.put("Hoodie", "Clothing");
+        CATEGORY_KEYWORDS.put("Shoes", "Clothing");
+        CATEGORY_KEYWORDS.put("pants", "Clothing");
+
+        CATEGORY_KEYWORDS.put("Concert", "Entertainment");
+        CATEGORY_KEYWORDS.put("Cinema", "Entertainment");
+        CATEGORY_KEYWORDS.put("Hobbies", "Entertainment");
     }
 
     @GetMapping("/category/{description}")
